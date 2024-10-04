55d0f13e4f7492a33dd831b8fb773fdc6f1e380d
Samuele Levizzari <128383918+samulevizz@users.noreply.github.com>
2024-10-04 21:51:10 +0200

    Update SwapVariabili.java

--- src/main/java/com/levizzari/SwapVariabili.java
+++ src/main/java/com/levizzari/SwapVariabili.java
@@ -1,37 +1,16 @@
 package com.levizzari;
 import java.util.Scanner;
 
-public class SwapVariabili 
-{
-public class SwapExample {
+public class Es2 {
     public static void main(String[] args) {
-        // Creazione dello scanner per input dell'utente
-        Scanner input = new Scanner(System.in);
-
-        // Chiede all'utente di inserire il valore di 'a'
-        System.out.print("Inserisci il valore di a: ");
-        int a = input.nextInt();
-
-        // Chiede all'utente di inserire il valore di 'b'
-        System.out.print("Inserisci il valore di b: ");
-        int b = input.nextInt();
-
-        // Stampa dei valori prima dello swap
-        System.out.println("Prima dello swap:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-
-        // Esegue lo swap usando la variabile 'c'
-        int c = a;
-        a = b;
-        b = c;
-
-        // Stampa dei valori dopo lo swap
-        System.out.println("Dopo lo swap:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.print("Dimmi l'eta': ");
+        Scanner in = new Scanner(System.in);
+        int eta = in.nextInt();
+        
+        if (eta >= 18) 
+            System.out.print("Sei maggiorenne \n");
+        else 
+            System.out.print("Sei minorenne \n");
+        in.close();
     }
 }
-
-}
-
