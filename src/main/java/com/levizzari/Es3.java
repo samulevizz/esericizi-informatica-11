5c28a0d9bb16bb61aea290e95aa7805e0ead60bc
Samuele Levizzari <128383918+samulevizz@users.noreply.github.com>
2024-10-04 21:47:38 +0200

    Update Es3.java

--- src/main/java/com/levizzari/Es3.java
+++ src/main/java/com/levizzari/Es3.java
@@ -8,24 +8,14 @@ public class Es3 {
         int age = in.nextInt();
         
         if (age >= 21) 
-        {
             System.out.print("In Kuwait puoi prendere la patente e in tutti gli altri stati del mondo \n");
-        } 
         else if (age >= 18) 
-        {
             System.out.print("In Italia e in altri stati nel mondo puoi prendere la patente \n");
-        } 
         else if (age >= 17) 
-        {
             System.out.print("In Francia e in America puoi prendere la patente \n");
-        } 
         else if (age >= 16) 
-        {
             System.out.print("In America puoi prendere la patente \n");
-        } 
         else 
-        {
             System.out.print("Non puoi prendere la patente \n");   
-        }
     }
 }
