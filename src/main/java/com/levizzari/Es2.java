9fd84591b98bb2b1701a720dde242a150135fd03
Samuele Levizzari <128383918+samulevizz@users.noreply.github.com>
2024-10-04 21:50:26 +0200

    Update Es2.java

--- src/main/java/com/levizzari/Es2.java
+++ src/main/java/com/levizzari/Es2.java
@@ -6,9 +6,9 @@ public class Es2 {
     public static void main(String[] args) {
         System.out.print("Dimmi l'eta': ");
         Scanner in = new Scanner(System.in);
-        int age = in.nextInt();
+        int eta = in.nextInt();
         
-        if (age >= 18) 
+        if (eta >= 18) 
             System.out.print("Sei maggiorenne \n");
         else 
             System.out.print("Sei minorenne \n");
